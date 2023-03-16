30348a9bbac875af6624b409172568a6b5c4c8cd
Rahul Tandon <64590382+rahul857-857@users.noreply.github.com>
2023-03-16 15:20:21 +0530

    Add files via upload

---
+++ CS_assignment/Program.cs
@@ -0,0 +1,49 @@
+﻿using assignmentmethods;
+namespace CS_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice = 0;
+            while (choice!=5)
+            {
+                Console.WriteLine("Please enter the relevant choice from 1 to 5");
+                Console.WriteLine("1. Get Cube of number till n number");
+                Console.WriteLine("2. Display the multiplication table vertically from 1 to n");
+                Console.WriteLine("3. Check whether a person is eligible for vote or not based on the age?");
+                Console.WriteLine("4. Check whether a person is eligible for admission in a professional course based on the PCM marks?");
+                Console.WriteLine("5. Quit the program");
+                choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        methods.calculateCube();
+                        break;
+                    case 2:
+                        methods.printMultiplicationTable();
+                        break;
+                    case 3:
+                        methods.isEligibleToVote();
+                        break;
+                    case 4:
+                        methods.isEligibleForAdmission();
+                        break;
+
+                    case 5:
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        Console.WriteLine("You've entered the wrong choice");
+                        break;
+                }
+                Console.WriteLine("--------------------------------------------------------------------------------------");
+            }
+            
+        }
+
+     
+    }
+}
---
+++ assignment1methods/methods.cs
@@ -0,0 +1,74 @@
+﻿namespace assignmentmethods
+{
+    public class methods
+    {
+        static public void calculateCube()
+        {
+            Console.WriteLine("Enter the number to get cube from 1 till the number:-");
+            int numberForCube = int.Parse(Console.ReadLine());
+            Console.WriteLine($"The cube of numbers from 1 till {numberForCube} are");
+            for (int i = 1; i <= numberForCube; i++)
+            {
+                Console.WriteLine(Math.Pow(i, 3));
+            }
+        }
+
+        static public void printMultiplicationTable()
+        {
+            Console.WriteLine("Enter the number to get multiplication table from 1 till the number:-");
+            int numberForMultiplication = int.Parse(Console.ReadLine());
+            Console.WriteLine("The multiplication table from 1 till n are:-");
+            for (int i = 1; i <= numberForMultiplication; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Table of {i}:-");
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.WriteLine($"{i} * {j} = {i * j}");
+
+                }
+            }
+        }
+
+
+        static public void isEligibleToVote()
+        {
+            Console.WriteLine("Enter the age:-");
+            int age = int.Parse(Console.ReadLine());
+            if (age >= 18)
+            {
+                Console.WriteLine("Eligible to vote");
+            }
+            else
+            {
+                Console.WriteLine("Not eligible to vote");
+            }
+        }
+
+        static public void isEligibleForAdmission()
+        {
+            Console.WriteLine("Enter your marks in Physics:-");
+            int marksInPhysics = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your marks in Chemistry:-");
+            int marksInChemistry = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your marks in Mathematics:-");
+            int marksInMaths = int.Parse(Console.ReadLine());
+            if (marksInPhysics >= 55 && marksInChemistry >= 50 && marksInMaths >= 65)
+            {
+                int totalMarks = marksInPhysics + marksInChemistry + marksInMaths;
+                if (totalMarks >= 180)
+                {
+                    Console.WriteLine("Eligible For Admission");
+                }
+                else
+                {
+                    Console.WriteLine("Not Eligible For Admission");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Not Eligible For Admission");
+            }
+        }
+    }
+}
